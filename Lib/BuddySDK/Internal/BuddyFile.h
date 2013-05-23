b3aea812b1dd0cfadd08f71afd58e7a80e871908
Shawn Burke <shawn@shawnburke.com>
2013-05-23 09:43:12 -0700

    * Missing BuddyFile contents

--- Lib/BuddySDK/Internal/BuddyFile.h
+++ Lib/BuddySDK/Internal/BuddyFile.h
@@ -7,7 +7,15 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <UIKit/UIImage.h>
 
 @interface BuddyFile : NSObject
+@property(nonatomic, retain) NSData* data;
+@property(nonatomic, retain) NSString* contentType;
+@property(nonatomic, retain) NSString* fileName;
+
+-(id)init;
+-(id)initWithImage:(UIImage*)image;
+-(id)initWithData:(NSData*)data contentType:(NSString*)contentType fileName:(NSString*) fileName;
 
 @end
