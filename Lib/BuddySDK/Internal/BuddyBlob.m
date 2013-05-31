f4d72980c13cecd00fa82d46a23421f55ea61b08
Ryan Brandenburg <ryanbrandenburg@Bradleys-MacBook-Pro.local>
2013-05-31 11:42:51 -0700

    Adding copyright and doc comments for Blob, Video and Sounds

--- Lib/BuddySDK/Internal/BuddyBlob.m
+++ Lib/BuddySDK/Internal/BuddyBlob.m
@@ -1,10 +1,18 @@
-//
-//  BuddyBlob.m
-//  BuddySDK
-//
-//  Created by Ryan Brandenburg on 5/15/13.
-//
-//
+/*
+ * Copyright (C) 2012 Buddy Platform, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 
 #import "BuddyBlobs.h"
 #import "BuddyBlob.h"
@@ -12,6 +20,10 @@
 #import "BuddyDataResponses_Exn.h"
 #import "BuddyWebWrapper.h"
 #import "BuddyClient_Exn.h"
+
+/// <summary>
+/// Represents a single blob object
+/// </summary>
 
 @implementation BuddyBlob
 
--- Lib/BuddySDK/Internal/BuddySounds.m
+++ Lib/BuddySDK/Internal/BuddySounds.m
@@ -1,15 +1,28 @@
-//
-//  BuddySounds.m
-//  BuddySDK
-//
-//  Created by Ryan Brandenburg on 5/23/13.
-//
-//
+/*
+ * Copyright (C) 2012 Buddy Platform, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
 
 #import "BuddySounds.h"
 #import "BuddyClient_Exn.h"
 #import "BuddyUtility.h"
 #import "BuddyWebWrapper.h"
+
+/// <summary>
+/// Represents a class that can be used to retrieve Sounds.
+/// </summary>
 
 @implementation BuddySounds
 
